4f7886dae15e08b56b83d85500a02ac59105ba8f
redzwantaufek <redzwantaufek2@gmail.com>
2023-03-23 12:39:36 +0800

    initial commit

---
+++ Product/product.php
@@ -0,0 +1,15 @@
+<aside id="filter">
+    <h3>Sort By:</h3>
+    <form>
+    <label>
+        <input type="radio" name="sort" value="price-asc"> Price: Low to High
+    </label>
+    <label>
+        <input type="radio" name="sort" value="price-desc"> Price: High to Low
+    </label>
+    <label>
+        <input type="checkbox" name="instock" value="true"> In Stock Only
+    </label>
+    <input type="submit" value="Apply">
+    </form>
+</aside>
